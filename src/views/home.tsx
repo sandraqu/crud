dddba2202bb90c99dd7ecbf541ef3b88c33d51d2
Gray Qu <378074+sandraqu@users.noreply.github.com>
2024-12-17 21:17:41 -0500

    add Add User button

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,13 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Users from "../components/Users";
-
+import { useNavigate } from "react-router-dom";
 export function HomeView() {
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ textAlign: "center" }}>
-      <h1>Hi!</h1>
+      <h1>'Ssup!</h1>
       <Typography sx={{ textDecoration: "green wavy underline overline" }}>
         Do your thing! Remember to ask questions!
       </Typography>
+      <Box
+        sx={{ display: "flex", gap: 2, justifyContent: "center", mt: 2, mb: 2 }}
+      >
+        <Button
+          variant="contained"
+          sx={{ width: "200px" }}
+          onClick={() => navigate("/form/1")}
+        >
+          Add User
+        </Button>
+      </Box>
       <Users />
     </Box>
   );
